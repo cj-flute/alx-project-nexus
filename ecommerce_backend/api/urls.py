f929d4f9915d67611a6b3cfe4b0e6cd0d61b0d05
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2025-11-20 23:22:58 +0100

    Create URLs for Authentication and views

---
+++ ecommerce_backend/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import RegisterView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('auth/register/', RegisterView.as_view(), name='auth_register'),
+    path('auth/login/', TokenObtainPairView.as_view(), name='login'),
+    path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
--- ecommerce_backend/api/views.py
+++ ecommerce_backend/api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import generics
+from django.contrib.auth import get_user_model
+from .serializers import RegisterSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
-# Create your views here.
+User = get_user_model()
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+
+class SomeProtectedView(APIView):
+    permission_classes = [IsAuthenticated]
